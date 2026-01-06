6530b0b356bb1b4d8f5049880775cc6450e8aefd
nolandebois7-spec <nolandebois7@gmail.com>
2026-01-06 21:33:19 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from datetime import datetime, timezone, timedelta
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_member_join(member):
+    age = datetime.now(timezone.utc) - member.created_at
+    if age < timedelta(days=15):
+        await member.kick(reason="Account too new")
+
+bot.run(os.environ[MTQ1ODE4ODg3MTMxMjYwNTM0NQ.GZL0VJ.cICLvmRzYXUYfCQ8u-E29O08VWdybkkbxACmXBg])
